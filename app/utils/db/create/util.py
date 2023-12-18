42956992ea840dff30ea594feeaeb6c17fee982b
Damion R Mounts <damionotg@gmail.com>
2023-12-18 13:21:34 -0500

    ATT&CK Citations: revert processing style to restore increasing cite nums

--- app/utils/db/create/util.py
+++ app/utils/db/create/util.py
@@ -4,19 +4,20 @@ import re
 def transform_description_citations(item):
     desc = item["description"]
 
+    cite_num = 1
     for markdown_citation, source_name in re.findall(r"(\(Citation: *(.+?) *\))", desc):
 
-        cite_num, ext_reference = next(
+        ext_reference = next(
             (
-                (index, ref)
-                for index, ref in enumerate(item["external_references"])
+                ref
+                for ref in item["external_references"]
                 if ref["source_name"].strip() == source_name
             ),
-            (None, None)
+            None
         )
 
         # not found -> clear it
-        if cite_num is None:
+        if ext_reference is None:
             desc = desc.replace(markdown_citation, "")
 
         # found
@@ -29,8 +30,9 @@ def transform_description_citations(item):
 
             # has url -> link it
             else:
-                safe_source_name = source_name.replace('"', '\\"')
+                safe_source_name = source_name.replace('"', '&quot;')
                 html_citation = f'<sup><a href="{url}" title="{safe_source_name}">[{cite_num}]</a></sup>'
                 desc = desc.replace(markdown_citation, html_citation)
+                cite_num += 1
 
     return desc
